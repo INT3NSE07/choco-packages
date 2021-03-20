5f8372ea0312d792f8e18465b5387b6195e47379
INT3NSE07 <d.jonathanpaul@gmail.com>
2021-03-20 12:09:43 +0530

    Add w10privacy

---
+++ w10privacy/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion"
+
+$packageArgs = @{
+  packageName    = 'w10privacy'
+  unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+  url64Bit       = 'https://sf91b3285d9193eec.jimcontent.com/download/version/1609175074/module/12302828636/name/W10Privacy.zip'
+  checksum64     = 'da79e51ead606708fb97473ffd4f83133ca6c6a36a723921783cdbfacac015e6'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyPackage -packageName 'w10privacy' -FileType 'exe' -SilentArgs '/S' -File64 "$toolsPath\W10Privacy $version Setup.exe"
